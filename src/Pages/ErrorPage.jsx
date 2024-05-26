38f8bb111472c313893256e836db669cc00f675a
Shahin-Mahmud98 <smshahinmahmud6@gmail.com>
2024-05-26 19:41:08 +0600

    new file added

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,16 @@
+import { Link, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+    const {error,status} = useRouteError();
+
     return (
-        <div>
-            <h1>Hello Error Element tunar kaj sudu error thekano thekao billah</h1>
+        <div className="container flex flex-col justify-center items-center h-screen text-center py-32">
+          <h1 className="text-7xl font-extrabold mb-8">Error {status || 404}</h1>  
+          <p className="lg-text-3xl">{error?.message}</p>
+          <button className="btn bg-red-500 text-white mt-8">
+            <Link to={'/'}>HomaPage</Link>
+          </button>
         </div>
     );
 };
